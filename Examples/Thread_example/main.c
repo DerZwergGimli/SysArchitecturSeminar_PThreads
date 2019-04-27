0d8cf45d42ffc82f9ca8a0c513d97da2046f4231
GMouaad <Mgsair@gmail.com>
2019-04-27 17:21:35 +0200

    upgraded to multiple Threads with Mutex Synch.

--- Examples/Thread_example/main.c
+++ Examples/Thread_example/main.c
@@ -1,15 +1,45 @@
 #include<stdio.h>
 #include<pthread.h>
 
+#include "main.h"
 
+// Parameter to pass to the Thread
+int count = 0;
+
+// Returned variable to the main routine
+int thread_rt = 0;
+
+// Thread Mutex
+pthread_mutex_t mutex; 
+
+// Thread info struc
+// Not used yet
+typedef struct
+{
+    pthread_t tid;
+    pthread_attr_t attr;
+    u_int16_t thread_num;
+} thread_struct;
+
+
+/**
+ * Thread routine
+ * 
+ * */
 void myThread(void * param){
     //TODO: Do smth useful
-    int mCount = *(int *) param;
-    mCount++;
+    //int mCount = *(int *) param;
+    //mCount++;
+    pthread_mutex_lock(&mutex);
 
-    printf("Count %d", mCount);
+    *(int *) param += 1 ;
+
+    printf("Count %d \n", *(int *) param);
+
+    pthread_mutex_unlock(&mutex);
+
     // pass the calue to the main process
-    pthread_exit(0);
+    pthread_exit((int *) param);
 }
 
 int main() {
@@ -17,21 +47,45 @@ int main() {
     // Thread ID
     pthread_t tid; 
 
+    // Create an Array of Thread ID's
+    pthread_t tidArray[MAX_NUM_OF_THREADS];
+
     // Thread Attributes: TCB : Thread Control Block
     pthread_attr_t attr;
+
+    // Create an Array of Thread Attributes (TCB)
+    pthread_attr_t attrArray[MAX_NUM_OF_THREADS];
+
+    // initialize all the Attributes
+    for(size_t i = 0; i < MAX_NUM_OF_THREADS; i++)
+    {
+        pthread_attr_init(attrArray+i);
+    }
     pthread_attr_init(&attr);
     
-    // Parameter to pass to the Thread
-    int count = 0;
+    
 
     // Start Thread
     pthread_create(&tid,        // Thread ID
                     &attr,      // TCB
                     myThread,   // function Callback (pointer) of the Thread
                     &count);    // parameter to pass to the Thread
-
+    
+    // Start all Threads
+    for(size_t i = 0; i < MAX_NUM_OF_THREADS; i++)
+    {
+        pthread_create(tidArray+i, attrArray+i, myThread, &count);
+        // NOTE: here, every Thread will run the same routine (function)
+    }
+    
+    
     // wait until Thread is done
-    pthread_join(&tid, 
-                NULL);  // param to capture the value passed by 
-	return 0;
+    for(size_t i = 0; i < MAX_NUM_OF_THREADS; i++)
+    {
+        pthread_join(&tidArray[i], 
+                &thread_rt);  // param to capture the value passed by 
+    }
+    
+    printf("Count returned: %d \n", thread_rt); // still returning 0 ..
+	return 0;   
 }
--- Examples/Thread_example/main.h
+++ Examples/Thread_example/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Prototypes */
 void myThread(void * param);
+
+/* Macros */
+#define MAX_NUM_OF_THREADS 100
 
 #endif /* MAIN_H */
